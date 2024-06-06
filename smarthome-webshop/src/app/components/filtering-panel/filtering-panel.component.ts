ece5d1a08b25b60ffc950cbcfc21d904288c4541
BruderJarck <ajaygaly@gmail.com>
2024-06-07 00:26:40 +0200

    product list odering by price and name, frontend

--- smarthome-webshop/src/app/components/filtering-panel/filtering-panel.component.ts
+++ smarthome-webshop/src/app/components/filtering-panel/filtering-panel.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
-import { ProductCategoryModel } from 'src/app/models';
+import { FormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { OrderingTypeModel, ProductCategoryModel } from 'src/app/models';
 import { ProductService } from 'src/app/shared/product.service';
 import { ProuctListService } from 'src/app/shared/prouct-list.service';
 
@@ -12,6 +16,13 @@ export class FilteringPanelComponent {
 
   categorys: ProductCategoryModel[] = []
   checkedCategorys: ProductCategoryModel[] = []
+  ordering_types: OrderingTypeModel[] = [
+    {displayname: 'Name Aufsteigend', name: 'name'},
+    {displayname: 'Name Absteigend', name: '-name'},
+    {displayname: 'Preis Aufsteigend', name: 'price'},
+    {displayname: 'Preis Absteigend', name: '-price'}
+  ]
+  selectedValue: string = ""
 
   constructor(
     private productService: ProductService,
@@ -33,6 +44,15 @@ export class FilteringPanelComponent {
       this.checkedCategorys = this.checkedCategorys.filter(cat => cat.id !== category.id);
     }
     this.productService.filterProducts(this.checkedCategorys).subscribe(
+      (res) => {
+        this.productListService.clearProducts()
+        this.productListService.addProduct(res)
+      }
+    )
+  }
+
+  orderingChanged(event:any){
+    this.productService.orderProducts(this.selectedValue).subscribe(
       (res) => {
         this.productListService.clearProducts()
         this.productListService.addProduct(res)
--- smarthome-webshop/src/app/shared/product.service.ts
+++ smarthome-webshop/src/app/shared/product.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@angular/core';
 import { AnyObject } from 'chart.js/types/basic';
 import { Observable, of } from 'rxjs';
 import { environment } from 'src/environments/environment';
-import { OrderModel, ProductCategoryModel as ProductCategoryModel, ProductModel } from '../models';
+import { OrderModel, OrderingTypeModel, ProductCategoryModel as ProductCategoryModel, ProductModel } from '../models';
 import { AccountService } from './account.service';
 
 @Injectable({
@@ -11,10 +11,12 @@ import { AccountService } from './account.service';
 })
 export class ProductService {
 
-  constructor(private http: HttpClient, private accService: AccountService) {}
+  constructor(private http: HttpClient, private accService: AccountService) { }
 
   private baseURL = environment.baseURL
   private productsURL = this.baseURL + 'products/';
+  private orderParam: string = "&odering=name"
+  private filterParam: string = ""
 
   httpOptions = {
     headers: new HttpHeaders({
@@ -52,21 +54,29 @@ export class ProductService {
     return this.http.get<ProductModel[]>(`${this.productsURL}?search=${term}`)
   }
 
-  filterProducts(categorys: ProductCategoryModel[]): Observable<ProductModel[]>{
+  filterProducts(categorys: ProductCategoryModel[]): Observable<ProductModel[]> {
+    const commaString = categorys.map(category => category.name).join(",")
+    this.filterParam = `category=${commaString}`
+    return this.http.get<ProductModel[]>(`${this.productsURL}?${this.filterParam}&${this.orderParam}`)
+  }
 
-    const commaString = categorys.map(category =>category.name).join(",")
-    return this.http.get<ProductModel[]>(`${this.productsURL}?category=${commaString}`)
-
+  orderProducts(ordering_type: string): Observable<ProductModel[]>{
+    this.orderParam = `ordering=${ordering_type}`
+    return this.http.get<ProductModel[]>(`${this.productsURL}?${this.filterParam}&${this.orderParam}`)
   }
 
   submitOrder(userId: number, productId: number): Observable<OrderModel> {
-    
+
     return this.http.post<OrderModel>(this.baseURL + "orders/", {
       "user": userId,
       "product": productId,
     })
   }
+
   getProductCategorys(): Observable<ProductCategoryModel[]> {
+
     return this.http.get<ProductCategoryModel[]>(this.baseURL + "categorys/")
   }
+
+
 }
