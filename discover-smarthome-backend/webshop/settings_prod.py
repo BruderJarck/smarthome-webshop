6b83e321c7c03ab1671e054d0d0f9fea1d99f434
BruderJarck <ajaygaly@gmail.com>
2024-06-10 19:45:04 +0200

    update some files for first deployment

--- discover-smarthome-backend/webshop/settings_prod.py
+++ discover-smarthome-backend/webshop/settings_prod.py
@@ -22,7 +22,7 @@ WHITENOISE_USE_FINDERS = True
 # DATABASES['default'].update(db_from_env)
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
