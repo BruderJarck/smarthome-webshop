c0d6b83bea93ae5b9473733fe2dec0826a513f10
BruderJarck <ajaygaly@gmail.com>
2024-06-12 09:25:00 +0200

    add sorting panel

---
+++ smarthome-webshop/src/app/components/sorting-panel/sorting-panel.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { OrderingTypeModel } from 'src/app/models';
+import { ProductService } from 'src/app/shared/product.service';
+
+@Component({
+  selector: 'app-sorting-panel',
+  templateUrl: './sorting-panel.component.html',
+  styleUrl: './sorting-panel.component.scss'
+})
+export class SortingPanelComponent {
+
+  ordering_types: OrderingTypeModel[] = [
+    {id: 1, displayname: 'Name Aufsteigend', name: 'name'},
+    {id: 2, displayname: 'Name Absteigend', name: '-name'},
+    {id: 3, displayname: 'Preis Aufsteigend', name: 'price'},
+    {id: 4, displayname: 'Preis Absteigend', name: '-price'}
+  ]
+  selectedValue: string = ""
+  
+  constructor(
+    private productService: ProductService
+  ) { }
+  
+  orderingChanged(event:any){
+    this.productService.orderProducts(this.selectedValue).subscribe()
+  }
+}
