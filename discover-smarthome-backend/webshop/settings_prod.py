725692d74a7691fee0c20fcd5f4556f0f98ee3ab
BruderJarck <ajaygaly@gmail.com>
2024-06-20 12:28:20 +0200

    add production file logging

--- discover-smarthome-backend/webshop/settings_prod.py
+++ discover-smarthome-backend/webshop/settings_prod.py
@@ -24,5 +24,21 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-# MEDIA_URL = '/media/'
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': './debug.log',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['file'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+    },
+}
